8f3dee7d03520ec5cf242b91ea93989889f39402
Sipeng He <hsp8412@hotmail.com>
2022-03-22 13:42:30 -0600

    Adjust the product card image size

--- src/components/productCard.jsx
+++ src/components/productCard.jsx
@@ -2,17 +2,41 @@ import React, { Component } from "react";
 import { Card, Button, Col } from "react-bootstrap";
 const ProductCard = (props) => {
   return (
+    // <Col>
+    //   <Card
+    //     className="d-flex justify-content-center"
+    //     style={{ width: "18rem", height: "30rem" }}
+    //   >
+    //     <Card.Img
+    //       variant="top"
+    //       src={props.product.image}
+    //       style={{ width: "15rem", height: "15rem" }}
+    //     />
+    //     <Card.Body className="d-flex flex-column">
+    //       <Card.Title className="mt-auto">{props.product.name}</Card.Title>
+    //       <Card.Text className="mt-auto">{props.product.description}</Card.Text>
+    //       <Button className="mt-auto" variant="primary">
+    //         Add To Cart
+    //       </Button>
+    //     </Card.Body>
+    //   </Card>
+    // </Col>
     <Col>
-      <Card style={{ width: "18rem", height: "30rem" }}>
-        <Card.Img variant="top" src={props.product.image} />
-        <Card.Body className="d-flex flex-column ">
-          <Card.Title>{props.product.name}</Card.Title>
-          <Card.Text>{props.product.description}</Card.Text>
-          <Button className="mt-auto" variant="primary">
+      <div className="card d-flex" style={{ width: "18rem", height: "30rem" }}>
+        <img
+          src={props.product.image}
+          className="card-img-top mx-auto"
+          alt="..."
+          style={{ height: "15rem" }}
+        />
+        <div className="card-body d-flex flex-column">
+          <h5 className="card-title mt-auto">{props.product.name}</h5>
+          <p className="card-text mt-auto">{props.product.description}</p>
+          <a href="#" className="btn btn-primary mt-auto">
             Add To Cart
-          </Button>
-        </Card.Body>
-      </Card>
+          </a>
+        </div>
+      </div>
     </Col>
   );
 };
