842179d88813f9d842252e04e9e5e0486bf231bb
Sipeng He <hsp8412@hotmail.com>
2022-04-14 21:19:34 -0600

    Fix layout of login page

--- front-end/src/pages/alreadyLoggedIn.jsx
+++ front-end/src/pages/alreadyLoggedIn.jsx
@@ -1,5 +1,6 @@
 import React from "react";
 import { useNavigate } from "react-router-dom";
+import { Container, Row } from "react-bootstrap";
 
 function handleLogOut(navigate) {
   localStorage.clear();
@@ -9,15 +10,21 @@ function handleLogOut(navigate) {
 const AlreadyLoggedIn = ({}) => {
   const navigate = useNavigate();
   return (
-    <div className="d-flex justify-content-center mt-3">
-      <h1>You have already logged in.</h1>
-      <button
-        type="button"
-        className="btn btn-primary btn-lg"
-        onClick={() => handleLogOut(navigate)}
-      >
-        Log out
-      </button>
+    <div>
+      <Container>
+        <div className="d-flex justify-content-center mt-3">
+          <h1>You have already logged in.</h1>
+        </div>
+        <div className="d-flex justify-content-center mt-3">
+          <button
+            type="button"
+            className="btn btn-primary btn-lg"
+            onClick={() => handleLogOut(navigate)}
+          >
+            Log out
+          </button>
+        </div>
+      </Container>
     </div>
   );
 };
