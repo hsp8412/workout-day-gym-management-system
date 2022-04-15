b3357caa8ae31c1db729b09d186572c11943e4bd
Sipeng He <hsp8412@hotmail.com>
2022-04-14 18:39:32 -0600

    Save progress

--- front-end/src/pages/login.jsx
+++ front-end/src/pages/login.jsx
@@ -1,17 +1,45 @@
 import React, { Component } from "react";
 import LoginForm from "../components/loginForm";
+import axios from "axios";
+import InvalidCredential from "../components/invalidCredential";
 
 class Login extends Component {
-  state = {};
+  state = { invalidCredential: false };
 
-  handleSubmit = (email, password, rememberMe) => {
-    console.log(email);
-    console.log(password);
-    console.log(rememberMe);
+  handleInvalidClose = () => {
+    this.setState({ invalidCredential: false });
+  };
+
+  handleSubmit = async (email, password, rememberMe) => {
+    // console.log(email);
+    // console.log(password);
+    // console.log(rememberMe);
+    await axios
+      .post("http://localhost:4000/auth", { email, password })
+      .then((res) => {
+        console.log(res.headers.data);
+        console.log(res.headers.data["x-token"]);
+        console.log(email);
+        console.log(password);
+      })
+      .catch(() => {
+        console.log("Invalid credential");
+        console.log(email);
+        console.log(password);
+        this.setState({ invalidCredential: true });
+      });
   };
 
   render() {
-    return <LoginForm onSubmit={this.handleSubmit} />;
+    return (
+      <div>
+        <LoginForm onSubmit={this.handleSubmit} />
+        <InvalidCredential
+          ifVisible={this.state.invalidCredential}
+          onClose={this.handleInvalidClose}
+        />
+      </div>
+    );
   }
 }
 
