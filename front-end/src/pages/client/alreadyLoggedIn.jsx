571c4d10fa9603d6100f09051607a50232b18a32
Sipeng He <hsp8412@hotmail.com>
2022-10-15 15:58:44 -0600

    change redirect route for logging out

--- front-end/src/pages/client/alreadyLoggedIn.jsx
+++ front-end/src/pages/client/alreadyLoggedIn.jsx
@@ -2,9 +2,9 @@ import React from "react";
 import { useNavigate } from "react-router-dom";
 import { Container } from "react-bootstrap";
 
-function handleLogOut() {
+function handleLogOut(navigate) {
   localStorage.clear();
-  window.location.reload();
+  navigate("/");
 }
 
 const AlreadyLoggedIn = ({}) => {
